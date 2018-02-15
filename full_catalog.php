d108a4d1d7b6da2efd5898dce494ea523b00849e
ardenebaxter <abaxter@rollins.edu>
2018-02-15 21:21:55 +0000

    added table query test

--- full_catalog.php
+++ full_catalog.php
@@ -13,4 +13,31 @@ if ($conn->connect_error){
 
 echo "Connected successfully!";
 
+// Attempt select query execution
+$sql = "SELECT Full_Catalog_Number,Baglog_Number FROM Catalog_tbl";
+if($result = $mysqli->query($sql)){
+    if($result->num_rows > 0){
+        echo "<table>";
+            echo "<tr>";
+                echo "<th>Catalog Number</th>";
+                echo "<th>Baglog Number</th>";
+            echo "</tr>";
+        while($row = $result->fetch_array()){
+            echo "<tr>";
+                echo "<td>" . $row['Full_Catalog_Number'] . "</td>";
+                echo "<td>" . $row['Baglog_Number'] . "</td>";
+            echo "</tr>";
+        }
+        echo "</table>";
+        // Free result set
+        $result->free();
+    } else{
+        echo "No records matching your query were found.";
+    }
+} else{
+    echo "ERROR: Could not able to execute $sql. " . $mysqli->error;
+}
+ 
+// Close connection
+$mysqli->close();
 ?>
