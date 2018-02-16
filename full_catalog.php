845ad67e288ea87b5a022aec5731158f5b6f0cec
Alexandra <alexir563@aol.com>
2018-02-16 04:03:12 +0000

    added columns to query

--- full_catalog.php
+++ full_catalog.php
@@ -18,15 +18,35 @@ echo "Connected successfully!";
 $sql = "SELECT Full_Catalog_Number,Baglog_Number FROM Catalog_tbl";
 if($result = $conn->query($sql)){
     if($result->num_rows > 0){
-        echo "<table>";
+        echo "<table class=\"table\">";
             echo "<tr>";
                 echo "<th>Catalog Number</th>";
-                echo "<th>Baglog Number</th>";
+                echo "<th>Size Fraction</th>";
+                echo "<th>Material</th>";
+                echo "<th>Material Type</th>";
+                echo "<th>Form</th>";
+                echo "<th>Decoration</th>";
+                echo "<th>Surface Treatment</th>";
+                echo "<th>Modification</th>";
+                echo "<th>Count</th>";
+                echo "<th>Weight</th>";
+                echo "<th>Culture Type</th>";
+                echo "<th>Notes</th>";
             echo "</tr>";
         while($row = $result->fetch_array()){
             echo "<tr>";
                 echo "<td>" . $row['Full_Catalog_Number'] . "</td>";
-                echo "<td>" . $row['Baglog_Number'] . "</td>";
+                echo "<td>" . $row['Size_Fraction'] . "</td>";
+                echo "<td>" . $row['Material'] . "</td>";
+                echo "<td>" . $row['Material_Type'] . "</td>";
+                echo "<td>" . $row['Form'] . "</td>";
+                echo "<td>" . $row['Decoration'] . "</td>";
+                echo "<td>" . $row['Surface_Treatment'] . "</td>";
+                echo "<td>" . $row['Modification'] . "</td>";
+                echo "<td>" . $row['Count'] . "</td>";
+                echo "<td>" . $row['Weight'] . "</td>";
+                echo "<td>" . $row['Culture_Type'] . "</td>";
+                echo "<td>" . $row['Notes'] . "</td>";
             echo "</tr>";
         }
         echo "</table>";
