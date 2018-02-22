cf4c9ae7286680c3117513fc7d4e30c8d02106b1
ardenebaxter <abaxter@rollins.edu>
2018-02-22 20:37:42 +0000

    Changed the nav bar links

--- index.php
+++ index.php
@@ -7,24 +7,6 @@
 <body>
     <?php include 'rollins_header.php';?>
     
-<<<<<<< HEAD
-    <div id="body_content" class="container-fluid">
-        <div class="row">
-          <div class="col-2" id="sidebar_section">
-            <nav class = "d-none d-md-block sidebar">
-                <div class="sidebar-sticky">
-                    <ul class = "nav flex-column">
-                        <li class = "nav-item active"><a class = "nav-link" href="#">Home</a></li>
-                        <li class = "nav-item"><a class = "nav-link" href="full_catalog.php">Full Catalog</a></li>
-                        <li class = "nav-item"><a class = "nav-link" href="#">Browse</a></li>
-                        <li class = "nav-item"><a class = "nav-link" href="#">Contact</a></li>
-                    </ul>
-                </div>
-            </nav>
-          </div>
-          <div class="col" id="content_section">
-              <p>Fusce vehicula, odio interdum viverra fringilla, dui lectus congue ante, finibus imperdiet leo est in quam. Nullam eu sapien gravid,  convallis purus vel, rutrum orci. Sed sapien urna, maximus non augue et, pulvinar condimentum nibh. Aliquam facilisis faucibus lacinia. Donec lectus nulla, tempus vitae faucibus eu, fermentum ut dui. Nunc ac ante pharetra dui viverra imperdiet vel eget mauris. Nullam fermentum ligula felis, tincidunt vestibulum velit sollicitudin eu. Donec tempor suscipit erat non gravida.</p>
-=======
    <div id="body_content" class="container-fluid">
      <div class="row">
         <div class="col-2" id="sidebar_section">
@@ -32,8 +14,8 @@
               <div class="sidebar-sticky">
                   <ul class = "nav flex-column">
                       <li class = "nav-item active"><a class = "nav-link" href="#">Home</a></li>
-                      <li class = "nav-item"><a class = "nav-link" href="browse.php">Browse</a></li>
-                      <li class = "nav-item"><a class = "nav-link" href="#">Search</a></li>
+                      <li class = "nav-item"><a class = "nav-link" href="full_catalog.php">Full Catalog</a></li>
+                      <li class = "nav-item"><a class = "nav-link" href="#">Browse</a></li>
                       <li class = "nav-item"><a class = "nav-link" href="#">Contact</a></li>
                   </ul>
               </div>
@@ -70,7 +52,6 @@
               <span class="carousel-control-next-icon"></span>
             </a>
             
->>>>>>> 35a1d489b247fcb3b2bc2f3bebb918ded6f9452b
           </div>
         </div>
       </div>
