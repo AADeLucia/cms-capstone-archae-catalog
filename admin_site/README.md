6d484a7412665700b45f2978a594c810ed24c05a
Drew Trautmann <Drew@Matthews-MacBook-Air.local>
2018-04-05 14:42:21 -0400

    structure of admin site

---
+++ admin_site/README.md
@@ -0,0 +1,2 @@
+# cms-capstone-archae-catalog
+Catalog created for the Rollins Archaeology department as a senior Computer Science Capstone project
---
+++ admin_site/footer.php
@@ -0,0 +1,6 @@
+<footer>
+    <div id="footer_container" class="container">
+        <h6 class="text-center">Rollins College</h5>
+        <h6 class="text-center">1000 Holt Avenue, Winter Park, FL 32789</h6>
+    </div>
+</footer>
