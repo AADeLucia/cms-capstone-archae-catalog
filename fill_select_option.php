c20f8a1bd8df64a11859bbde6a5d427406a77795
Alexir563 <alexir563@aol.com>
2018-04-19 16:33:27 -0400

    query in alphabetical order

--- fill_select_option.php
+++ fill_select_option.php
@@ -12,7 +12,7 @@ if ($conn->connect_error){
 }
 
 // Attempt select query execution
-$sql = "SELECT DISTINCT " . $column . " FROM " . $table;
+$sql = "SELECT DISTINCT " . $column . " FROM " . $table . " ORDER BY " . $column;
 if($result = $conn->query($sql)){
     if($result->num_rows > 0){
         while($row = $result->fetch_array()){
