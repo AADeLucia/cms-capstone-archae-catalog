1645add076370736516b6dd3e6786a83ca06e097
Arden Baxter <ardenbaxter@Ardens-MacBook-Air-2.local>
2018-04-19 16:06:20 -0400

    Changed name of title

--- rollins_header.php
+++ rollins_header.php
@@ -2,8 +2,8 @@
     <div id="head_container" class="container-fluid">
         <div class="row align-items-center">
             <div class="col-lg-10 col-md-9 col-sm-12 col-xs-12">
-                <h1 class="display-4" id="header">RCAL Collection</h1>
-                <h6 id = "subheader">Rollins College Archaeology Lab Collection</h5>
+                <h1 class="display-4" id="header">RCAL Collections</h1>
+                <h6 id = "subheader">Rollins College Archaeology Lab Collections</h5>
             </div>
             <div class="col-lg-2 col-md-3 d-none d-md-block">
                 <img id="rollins_logo" src="media/rollins.png">
