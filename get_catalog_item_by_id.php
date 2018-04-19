2f19c1cd66f82242588dd9fd50ebbc53e386cf15
Alexir563 <alexir563@aol.com>
2018-04-19 16:21:08 -0400

    fixed merge issue

--- get_catalog_item_by_id.php
+++ get_catalog_item_by_id.php
@@ -1,6 +1,6 @@
 <?php
-//include "local_db_access.php";
-include "remote_db_access.php";
+include "local_db_access.php";
+//include "remote_db_access.php";
 
 //Create connection
 $conn = new mysqli($servername, $username, $password, $database);
@@ -10,63 +10,11 @@ if ($conn->connect_error){
     die("Connection failed: " .$conn->connect_error);
 }
 
-<<<<<<< HEAD
 // Prepare
 $sql = "SELECT * FROM catalog WHERE full_catalog_number=". "\"". $catalog_number ."\"";
 
 // Execute statement and print rows
 include "build_cards_user_view.php";
-=======
-// Prepare and bind statement
-$stmt = $conn->prepare("SELECT * FROM Catalog_tbl WHERE Catalog_Number=?");
-$stmt->bind_param("s", $catalog_number);
-
-$catalog_number = %_POST['catalog_number'];
-
-// Attempt statement execution
-if($result = $stmt->execute()){
-echo nl2br("\n" . "Returned " . $result->num_rows . " rows");
-    if($result->num_rows > 0){
-        while($row = $result->fetch_array()){
-            echo "<div class=\"card w-100\">";
-            echo "<div class=\"container\">";
-            echo "<div class=\"row align-items-center\">";
-            echo "<div id =\"catalog_picture\" class=\"col\">";
-              echo "<img class=\"catalog_thumbnail\" src=\"rollins_crest.png\">";
-            echo "</div>";
-            echo "<div class=\"col\">";
-              echo "<ul class=\"list-group list-group-flush\">";
-                echo "<li class=\"list-group-item\"><span class=\"catalog_field\">Catalog Number</span>: " . $row['Full_Catalog_Number'] . "</li>";
-                echo "<li class=\"list-group-item\"><span class=\"catalog_field\">Material</span>: " . $row['Material'] . "</li>";
-                echo "<li class=\"list-group-item\"><span class=\"catalog_field\">Material Type</span>: " . $row['Material_Type'] . "</li>";
-                echo "<li class=\"list-group-item\"><span class=\"catalog_field\">Form</span>: " . $row['Form'] . "</li>";
-                echo "<li class=\"list-group-item\"><span class=\"catalog_field\">Decoration</span>: " . $row['Decoration'] .  "</li>";
-                echo "</ul>";
-            echo "</div>";
-            echo "<div class=\"col\">";
-              echo "<ul class=\"list-group list-group-flush\">";
-              echo "<li class=\"list-group-item\"><span class=\"catalog_field\">Surface Treatment</span>: " . $row['Surface_Treatment'] . "</li>";
-              echo "<li class=\"list-group-item\"><span class=\"catalog_field\">Modification</span>: " . $row['Modification'] . "</li>";
-              echo "<li class=\"list-group-item\"><span class=\"catalog_field\">Count</span>: " . $row['Count (n)'] . "</li>";
-              echo "<li class=\"list-group-item\"><span class=\"catalog_field\">Weight</span>: " . $row['Weight (g)'] . "</li>";
-              echo "<li class=\"list-group-item\"><span class=\"catalog_field\">Culture Type</span>: " . $row['Culture_Type'] . "</li>";
-              echo "</ul>";
-            echo "</div>";
-            echo "</div>";
-            echo "</div>";
-            echo "</div>";
-            echo "<br />";
-        }
-        // Free result set
-        $result->free();
-        $stmt->close();
-    } else{
-        echo "\nNo records matching your query were found.";
-    }
-} else{
-    echo "<br />ERROR: Could execute \"$sql\". " . $conn->error;
-}
->>>>>>> 92400a3e0e5957af5eb5c333ebf4b9373efb670f
 
 // Close connection
 $conn->close();
