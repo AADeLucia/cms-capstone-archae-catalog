ac4dd9891cbb7c20d2578521677a7d65017d6ed0
Alexir563 <alexir563@aol.com>
2018-04-19 16:44:06 -0400

    fixed search

---
+++ admin_site/get_catalog_item_by_id.php
@@ -0,0 +1,21 @@
+<?php
+//include "local_db_access.php";
+include "remote_db_access.php";
+
+//Create connection
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error){
+    die("Connection failed: " .$conn->connect_error);
+}
+
+// Prepare
+$sql = "SELECT * FROM catalog WHERE full_catalog_number=". "\"". $catalog_number ."\"";
+
+// Execute statement and print rows
+include "build_table_admin_view.php";
+
+// Close connection
+$conn->close();
+?>
