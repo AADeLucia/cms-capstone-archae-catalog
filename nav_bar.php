09b4bfa1018c0a21f5e1bf3a379db057e585ba61
Arden Baxter <ardenbaxter@Ardens-MacBook-Air-2.local>
2018-04-21 20:28:34 -0400

    Updated the link for Home

---
+++ nav_bar.php
@@ -0,0 +1,10 @@
+<nav class = "sidebar">
+    <div class="sidebar-sticky">
+        <ul class = "nav flex-column">
+            <li class = "nav-item active"><a class = "nav-link" href="index.php">Home</a></li>
+            <li class = "nav-item"><a class = "nav-link" href="full_catalog.php">Full Catalog</a></li>
+            <li class = "nav-item"><a class = "nav-link" href="browse.php">Browse</a></li>
+            <li class = "nav-item"><a class = "nav-link" href="contact_form.php">Contact</a></li>
+        </ul>
+    </div>
+</nav>
