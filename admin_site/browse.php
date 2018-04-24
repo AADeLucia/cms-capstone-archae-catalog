569780cecc9ebd78c2bd20b406ebcc40318d3a3e
Alexir563 <alexir563@aol.com>
2018-04-24 17:21:08 -0400

    fixed bug in browse

--- admin_site/browse.php
+++ admin_site/browse.php
@@ -1,115 +1,118 @@
 <!DOCTYPE html>
 <html>
-    <?php include 'head.php';?>
-    <head>
-        <title>Browse</title>
-    </head>
+<?php include 'head.php';?>
+<head>
+  <title>Browse</title>
+</head>
 <body>
-    <?php include 'rollins_header.php';?>
+  <?php include 'rollins_header.php';?>
 
-    <div id="body_content" class="container-fluid">
-        <div class="row">
-          <div class="col-2" id="sidebar_section">
-            <?php include 'admin_nav_bar.php';?>
+  <div id="body_content" class="container-fluid">
+    <div class="row">
+      <div class="col-2" id="sidebar_section">
+        <?php include 'admin_nav_bar.php';?>
+      </div>
+      <div class="col" id="content_section">
+        <!-- Search by Catalog Number -->
+        <form class="form-inline" method="post" action="">
+          <div class="form-group mb-2">
+            Catalog Number:
           </div>
-          <div class="col" id="content_section">
-            <!-- Search by Catalog Number -->
-            <form class="form-inline" method="post" action="">
-              <div class="form-group mb-2">
-                Catalog Number:
-              </div>
-              <div class="form-group mx-sm-3 mb-2">
-                <input type="text" class="form-control" name="catalog_number">
-              </div>
-                <button type="submit" name="search_submit" class="btn btn-primary">Search</button>
-            </form>
-            <hr />
+          <div class="form-group mx-sm-3 mb-2">
+            <input type="text" class="form-control" name="catalog_number">
+          </div>
+          <button type="submit" name="search_submit" class="btn btn-primary">Search</button>
+        </form>
+        <hr />
 
-            <!-- Browse -->
-            Define your search: (Hold CTRL on Windows or CMD on Mac to select more than one)
-            <form method="post" action="">
-              <div class="row align-items-center">
-                <div class="form-group col">
-                  <label for="material">Material</label>
-                  <select multiple name="materials[]" class="form-control">
-                    <option selected>---All---</option>
-                    <?php $table="materials"; $column="material_name"; include "fill_select_option.php"; ?>
-                  </select>
-                </div>
-                <div class="form-group col">
-                  <label for="material_type">Material Type</label>
-                  <select multiple name="material_types[]" class="form-control">
-                    <option selected>---All---</option>
-                    <?php $table="material_types"; $column="type_name"; include "fill_select_option.php"; ?>
-                  </select>
-                </div>
-                <div class="form-group col">
-                  <label for="form">Form</label>
-                  <select multiple name="forms[]" class="form-control">
-                    <option selected>---All---</option>
-                    <?php $table="forms"; $column="form_name"; include "fill_select_option.php"; ?>
-                  </select>
-                </div>
-                <div class="form-group col">
-                  <label for="surface_treatment">Surface Treatment</label>
-                  <select multiple name="surface_treatments[]" class="form-control">
-                    <option selected>---All---</option>
-                    <?php $table="surface_treatments"; $column="treatment_name"; include "fill_select_option.php"; ?>
-                  </select>
-                </div>
-                <div class="form-group col">
-                  <label for="decoration">Decoration</label>
-                  <select multiple name="decorations[]" class="form-control">
-                    <option selected>---All---</option>
-                    <?php $table="decorations"; $column="decoration_name"; include "fill_select_option.php"; ?>
-                  </select>
-                </div>
-                <div class="form-group col">
-                  <label for="modification">Modification</label>
-                  <select multiple name="modifications[]" class="form-control">
-                    <option selected>---All---</option>
-                    <?php $table="modifications"; $column="modification_name"; include "fill_select_option.php"; ?>
-                  </select>
-                </div>
-              </div>
-              <div class="row align-items-center">
-                <div class="col">
-                  <button type="submit" name="browse_submit" class="btn btn-primary">Search</button>
-                </div>
-              </div>
-            </form>
-
-            <hr />
-
-            <!-- Show results -->
-            <div id="browse_results">
-              <?php
-              // Show catalog item search results
-                if(isset($_POST["search_submit"])){
-                  $catalog_number = $_POST["catalog_number"];
-                  include 'get_catalog_item_by_id.php';
-                }
-              // Show browse results
-                else if(isset($_POST["browse_submit"])){
-                  // Save select form values into array
-                  $default_select = "---All---";
-                  $selections = array("material"=>$_POST["materials"],
-                  "material_type"=>$_POST["material_types"], "form"=>$_POST["forms"],
-                "surface_treatment"=>$_POST["surface_treatments"], "modification"=>$_POST["modifications"],
-              "decoration"=>$_POST["decorations"]);
-                // Check if selections were made in all forms
-                foreach ($selections as $key => $val){
-                  if ($val[0] == $default_select){
-                    $selections[$key] = NULL;
-                  }
-                }
-                // Display results
-                  include 'browse_database.php';
-                }?>
+        <!-- Browse -->
+        Define your search: (Hold CTRL on Windows or CMD on Mac to select more than one)
+        <form method="post" action="">
+          <div class="row align-items-center">
+            <div class="form-group col">
+              <label for="material">Material</label>
+              <select multiple name="materials[]" class="form-control">
+                <option selected>---All---</option>
+                <?php $table="materials"; $column="material_name"; include "fill_select_option.php"; ?>
+              </select>
+            </div>
+            <div class="form-group col">
+              <label for="material_type">Material Type</label>
+              <select multiple name="material_types[]" class="form-control">
+                <option selected>---All---</option>
+                <?php $table="material_types"; $column="type_name"; include "fill_select_option.php"; ?>
+              </select>
+            </div>
+            <div class="form-group col">
+              <label for="form">Form</label>
+              <select multiple name="forms[]" class="form-control">
+                <option selected>---All---</option>
+                <?php $table="forms"; $column="form_name"; include "fill_select_option.php"; ?>
+              </select>
+            </div>
+            <div class="form-group col">
+              <label for="surface_treatment">Surface Treatment</label>
+              <select multiple name="surface_treatments[]" class="form-control">
+                <option selected>---All---</option>
+                <?php $table="surface_treatments"; $column="treatment_name"; include "fill_select_option.php"; ?>
+              </select>
+            </div>
+            <div class="form-group col">
+              <label for="decoration">Decoration</label>
+              <select multiple name="decorations[]" class="form-control">
+                <option selected>---All---</option>
+                <?php $table="decorations"; $column="decoration_name"; include "fill_select_option.php"; ?>
+              </select>
+            </div>
+            <div class="form-group col">
+              <label for="modification">Modification</label>
+              <select multiple name="modifications[]" class="form-control">
+                <option selected>---All---</option>
+                <?php $table="modifications"; $column="modification_name"; include "fill_select_option.php"; ?>
+              </select>
             </div>
           </div>
+          <div class="row align-items-center">
+            <div class="col">
+              <button type="submit" name="browse_submit" class="btn btn-primary">Search</button>
+            </div>
+          </div>
+        </form>
+
+        <hr />
+
+        <!-- Show results -->
+        <div id="browse_results">
+          <?php
+          // Show catalog item search results
+          if(isset($_POST["search_submit"])){
+            $catalog_number = $_POST["catalog_number"];
+            include 'get_catalog_item_by_id.php';
+          }
+          // Show browse results
+          else if(isset($_POST["browse_submit"])){
+            // Save select form values into array
+            $default_select = "---All---";
+            $is_empty = True;
+            $selections = array("material"=>$_POST["materials"],
+            "material_type"=>$_POST["material_types"], "form"=>$_POST["forms"],
+            "surface_treatment"=>$_POST["surface_treatments"], "modification"=>$_POST["modifications"],
+            "decoration"=>$_POST["decorations"]);
+            // Check if selections were made in all forms
+            foreach ($selections as $key => $val){
+              if ($val[0] == $default_select){
+                $selections[$key] = NULL;
+              } else {
+                $is_empty = False;
+              }
+            }
+            // Display results
+            include 'browse_database.php';
+          }?>
         </div>
+      </div>
     </div>
+  </div>
 
 </body>
 <?php include 'footer.php';?>
--- admin_site/browse_database.php
+++ admin_site/browse_database.php
@@ -1,41 +1,45 @@
 <?php
 include "connect_to_database.php";
-// Create individual statements
-$stmts = array();
-foreach ($selections as $field=>$values){
-  // Build a select statement for each field query
-  if ($values) {
-    $stmt = "SELECT * FROM catalog WHERE " . $field . " in (";
-    echo "$stmt";
-    foreach ($values as $v){
-      $stmt .= "\"$v\",";
+
+if ($is_empty){
+  $sql = "SELECT * FROM catalog";
+} else {
+  // Create individual statements
+  $stmts = array();
+  foreach ($selections as $field=>$values){
+    // Build a select statement for each field query
+    if ($values) {
+      $stmt = "SELECT * FROM catalog WHERE " . $field . " in (";
+      foreach ($values as $v){
+        $stmt .= "\"$v\",";
+      }
+      $stmt = substr($stmt, 0, -1) . ")"; // remove last comma
+      $stmts[] = $stmt;
     }
-    $stmt = substr($stmt, 0, -1) . ")"; // remove last comma
-    $stmts[] = $stmt;
-    echo "$stmt";
   }
+  // Join the statements into one query
+  $sql = "SELECT * FROM ";
+  $table_counter = 1;
+
+  foreach ($stmts as $s){
+    $sql .= "(" . $s . ") as t" . $table_counter . " INNER JOIN ";
+    $table_counter++;
+  }
+  // Remove the last "inner join"
+  $sql = substr($sql, 0, -12);
+
+  // Finish query
+  $sql .= " on ";
+  for ($i=1; $i < $table_counter-1; $i++){
+    $sql .= " t" . ($i) . ".full_catalog_number=t" . ($i+1) . ".full_catalog_number AND ";
+  }
+  // Remove last "and"
+  $sql = substr($sql, 0, -5);
 }
-// Join the statements into one query
-$sql = "SELECT * FROM ";
-$table_counter = 1;
-echo "$sql";
-foreach ($stmts as $s){
-  $sql .= "(" . $s . ") as t" . $table_counter . " INNER JOIN ";
-  echo "$sql";
-  $table_counter++;
-}
-// Remove the last "inner join"
-$sql = substr($sql, 0, -12);
-echo "$sql";
-// Finish query
-$sql .= " on ";
-for ($i=1; $i < $table_counter-1; $i++){
-  $sql .= " t" . ($i) . ".full_catalog_number=t" . ($i+1) . ".full_catalog_number AND ";
-}
-// Remove last "and"
-$sql = substr($sql, 0, -5);
+
 // Execute statement and print rows
 include "build_table_admin_view.php";
+
 // Close connection
 $conn->close();
 ?>
