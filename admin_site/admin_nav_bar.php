03c7d060717da9bebe66c1d0f341a959286b2ba4
Arden Baxter <ardenbaxter@Ardens-MacBook-Air-2.local>
2018-04-26 17:28:01 -0400

    Updated the catalog procedure page

--- admin_site/admin_nav_bar.php
+++ admin_site/admin_nav_bar.php
@@ -2,7 +2,7 @@
     <div class="sidebar-sticky">
         <ul class = "nav flex-column">
             <!-- <li class = "nav-item active"><a class = "nav-link" href="index.php">Home</a></li> -->
-            <li class = "nav-item"><a class = "nav-link" href="index.php">Full Catalog</a></li>
+            <li class = "nav-item"><a class = "nav-link" href="index.php">Catalog</a></li>
             <li class = "nav-item"><a class = "nav-link" href="browse.php">Browse</a></li>
             <li class = "nav-item"><a class = "nav-link" href="baglog_browse.php">Bag Log</a></li>
             <li class = "nav-item"><a class = "nav-link" href="add_entry.php">Add Catalog Entry</a></li>
@@ -10,6 +10,7 @@
             <li class = "nav-item"><a class = "nav-link" href="references.php">References</a></li>
             <li class = "nav-item"><a class = "nav-link" href="recently_deleted_database.php">Recently Deleted</a></li>
             <li class = "nav-item"><a class = "nav-link" href="front_page_maintenance.php">Front Page Maintenance</a></li>
+            <li class = "nav-item"><a class = "nav-link" href="catalog_procedure.php">Catalog Procedure</a></li>
         </ul>
     </div>
 </nav>
---
+++ admin_site/catalog_procedure.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+    <?php include 'head.php';?>
+    <head>
+        <title>Catalog Procedure</title>
+    </head>
+<body>
+    <?php include 'rollins_header.php';?>
+
+    <div id="body_content" class="container-fluid">
+    <div class="row">
+          <div class="col-lg-2 col-md-1 col-sm-1 col-xs-1" id="sidebar_section">
+            <?php include 'admin_nav_bar.php';?>
+          </div>
+
+          <div class="col-lg-10 col-md-11 col-sm-11 col-xs-11" id="content_section">
+            <h1>Catalog Procedure</h1>
+              <h5><u>You will need:</u></h5>
+              <ul style="list">
+                <li>A provenience of artifacts to catalog (ex. TU 1, Level B, Bag Log number 8OR471.2)</li>
+                <li>Cataloging kit (4 mil bags, catalog tags, sharpies, catalog sheets, pens)</li>
+                <li>Trays</li>
+                <li>Scale</li>
+                <li>½ inch geological screen (for separating crumb sherds)</li>
+                <li>The appropriate catalog binder for the site you are working on</li>
+                <li>The Cataloging Procedure Manual</li>
+              </ul>
+              
+          </div>
+    </div>
+    </div>
+
+</body>
+<?php include 'footer.php';?>
+</html>
