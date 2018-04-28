a84ae5eee8d9eed39ebf4c947c1ea7b706922a4d
chadgaines <ChadGaines@chads-MacBook-Pro.local>
2018-04-28 16:11:02 -0400

    Completed baglog addition to catalog, fixed minor issues with names in the bag log entry form.

--- admin_site/bag_entry_form.php
+++ admin_site/bag_entry_form.php
@@ -1,24 +1,24 @@
 <h1>New Bag Log Entry</h1>
 <h6>Please fill all required fields</h6>
+<form action="add_bag_to_baglog.php" method="post" enctype="multipart/form-data">
 
-<form>
 <div class="row">
   <div class="col">
     <div class="form-group">
-      <label for="full_baglog_num">Full Baglog Number</label>
-      <input type="text" class="form-control" id="full_baglog_num">
+      <label for="full_baglog_num">Full Baglog Number*</label>
+      <input type="text" class="form-control" name="full_baglog_num" required>
     </div>
   </div>
   <div class="col">
     <div class="form-group">
       <label for="common_name">Common Name</label>
-      <input type="text" class="form-control" id="common_name">
+      <input type="text" class="form-control" name="common_name">
     </div>
   </div>
   <div class="col">
     <div class="form-group">
       <label for="state_site_num">State Site Number</label>
-      <input type="text" class="form-control" id="state_site_num">
+      <input type="text" class="form-control" name="state_site_num">
     </div>
   </div>
 </div>
@@ -27,19 +27,19 @@
   <div class="col">
     <div class="form-group">
       <label for="site_baglog_num">Site Baglog Number</label>
-      <input type="number" class="form-control" id="site_baglog_num">
+      <input type="number" class="form-control" name="site_baglog_num">
     </div>
   </div>
   <div class="col">
     <div class="form-group">
       <label for="provenience">Provenience</label>
-      <input type="text" class="form-control" id="provenience">
+      <input type="text" class="form-control" name="provenience">
     </div>
   </div>
   <div class="col">
     <div class="form-group">
       <label for="level_inches">Level (inches)</label>
-      <input type="text" class="form-control" id="level_inches">
+      <input type="text" class="form-control" name="level_inches">
     </div>
   </div>
 </div>
@@ -48,19 +48,19 @@
   <div class="col">
     <div class="form-group">
       <label for="provenience_type">Provenience Type</label>
-      <input type="text" class="form-control" id="provenience_type">
+      <input type="text" class="form-control" name="provenience_type">
     </div>
   </div>
   <div class="col">
     <div class="form-group">
       <label for="recovery_inches">Recovery (inches)</label>
-      <input type="number" class="form-control" id="recovery_inches">
+      <input type="number" class="form-control" name="recovery_inches">
     </div>
   </div>
   <div class="col">
     <div class="form-group">
       <label for="associated_provenience">Associated Provenience</label>
-      <input type="text" class="form-control" id="associated_provenience">
+      <input type="text" class="form-control" name="associated_provenience">
     </div>
   </div>
 </div>
@@ -69,13 +69,19 @@
   <div class="col">
     <div class="form-group">
       <label for="excavation_date">Excavation Date</label>
-      <input type="number" class="form-control" id="excavation_date">
+      <input type="number" class="form-control" name="excavation_date">
+    </div>
+  </div>
+  <div class="col">
+    <div class="form-group">
+      <label for="excavation_date">Excavator's Initials</label>
+      <input type="number" class="form-control" name="excavator_initials">
     </div>
   </div>
   <div class="col">
     <div class="form-group">
       <label for="unit_size">Unit Size</label>
-      <input type="text" class="form-control" id="unit_size">
+      <input type="text" class="form-control" name="unit_size">
     </div>
   </div>
   <div class="col">
@@ -86,13 +92,13 @@
   <div class="col">
     <div class="form-group">
       <label for="issue">Issue</label>
-      <input type="checkbox" class="form-control" id="issue">
+      <input type="checkbox" class="form-control" name="issue">
     </div>
   </div>
   <div class="col">
     <div class="form-group">
       <label for="issue_defined">Issue Defined</label>
-      <input type="text" class="form-control" id="issue_defined">
+      <input type="text" class="form-control" name="issue_defined">
     </div>
   </div>
   <div class="col">
@@ -100,7 +106,7 @@
 </div>
 
 <div>
-  <button type="submit" class="btn btn-primary" action="baglog_add_sql.php">Submit</button>
+  <button type="submit" class="btn btn-primary">Submit</button>
 </div>
 
 </form>
